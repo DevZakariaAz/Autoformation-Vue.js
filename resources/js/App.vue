aca47baa8360c6a0eaa7f4cfd0e6abf1fa52ad2c
Dev Zakaria <Azizi.zakaria.solicode@gmail.com>
2025-01-24 15:19:23 +0100

    Tuto 2 done

---
+++ resources/js/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <router-view v-slot="{ Component, route }">
+        <div :key="route.name">
+            <Component :is="Component" />
+        </div>
+    </router-view>
+</template>
+<script setup>
+import axios from "axios";
+import { ref } from "vue";
+
+const response = ref();
+
+const getValue = async () => {
+    try {
+        response.value = await axios.get("/api/test-me");
+    } catch (error) {
+        // Do something with the error
+        console.log(error);
+    }
+};
+
+</script>
